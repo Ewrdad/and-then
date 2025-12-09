bedb034378fc6f4d13f7da17296c902bb19b3a42
Ewrdad <Ewrdad12+git@gmail.com>
2025-12-09 22:24:24 +0000

    chore(types): remove .d.ts and rely on in-file JSDoc types; update imports and tests

--- Lib/StartScenario.test.js
+++ Lib/StartScenario.test.js
@@ -3,12 +3,13 @@ import { startScenario } from "./StartScenario";
 
 describe("start scenario", () => {
   test("basic startScenario function", async () => {
-    const scenario = startScenario({
+    const scenario = await startScenario({
       name: "Test Scenario",
       scenarioFileVersion: 1,
       meta: { description: "A test scenario" },
       situations: [],
     });
+    console.log(scenario);
     expect(scenario.loading).toBe(true);
   });
 });
