3d5b33ae974ca38d896f810b49d4ef57c6463afb
Ewrdad <Ewrdad12+git@gmail.com>
2025-12-09 22:03:51 +0000

    docs: annotate startScenario param with Scenario JSDoc type

---
+++ Lib/StartScenario.js
@@ -0,0 +1,8 @@
+/**
+ * MARK: startScenario
+ * @param {import('../Standard/scenario.js').Scenario} scenario - A scenario object conforming to the standard format
+ * @returns {void}
+ */
+export const startScenario = (scenario) => {
+  console.log(`Starting scenario: ${scenario.name}`);
+};
